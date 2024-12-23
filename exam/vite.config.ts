248ac21020912cc17cfa175c21f6a5e68ac8d8c6
Yaroslav <yarmkhlv@gmail.com>
2024-12-23 21:05:18 +0300

    fix vite config

--- exam/vite.config.ts
+++ exam/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: '/2024-2-VK-EDU-Frontend-Y-Mikhailov/',
+    plugins: [react()],
+});
